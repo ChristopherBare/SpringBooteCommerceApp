1e153672a5997d018b0a845754ebb3b4426b03da
Christopher Bare <christopherbare@outlook.com>
2019-05-02 15:41:46 -0400

    Updated htnl page to serve hello world bootstrap page. This works now.

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String main(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "main";
