c195e7eb5766fc43977c465704ed6d2c11d830d0
Christopher Bare <christopherbare@outlook.com>
2019-05-08 10:23:58 -0400

    Adding the model

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,98 @@
+package com.example.demo.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+    private double price;
+    private int quantity, rating;
+    private String description, name, brand;
+
+    public Product(double price, int quantity, String description, String name, String brand, int rating) {
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+        this.name = name;
+        this.brand = brand;
+        this.rating = rating;
+    }
+
+    public Product() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", description='" + description + '\'' +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+}
