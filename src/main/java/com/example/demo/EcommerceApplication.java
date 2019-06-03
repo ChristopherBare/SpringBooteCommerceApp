91b18551a64ef41b57ec410c0a000c581f1a23ed
Christopher Bare <christopherbare@outlook.com>
2019-06-03 15:44:57 -0400

    push

--- src/main/java/com/example/demo/EcommerceApplication.java
+++ src/main/java/com/example/demo/EcommerceApplication.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import com.example.demo.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaRepositories("com.example.demo.model")
 public class EcommerceApplication {
 
     public static void main(String[] args) {
